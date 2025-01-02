d9c9e95a4caf252ef170b7dfb8bb2038aff2b6ac
gauriii1024 <gauri10022004@gmail.com>
2025-01-02 18:44:14 +0530

    Signup + Login api calls done

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import userRoute from './routes/user.route.js'
 import connectDB from "./config/database.js";
+import cors from "cors"
 import messageRoute from "./routes/message.route.js"
 dotenv.config({});
 
@@ -12,10 +13,17 @@ dotenv.config({});
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 
+app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cookieParser())
+
+const corsOption={
+    origin:'http://localhost:3000',
+    credentials: true
+};
+app.use(cors(corsOption))
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/message", messageRoute);
--- frontend_/src/components/SignUp.jsx
+++ frontend_/src/components/SignUp.jsx
@@ -1,8 +1,108 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const SignUp = () => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState({
+    fullName: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    gender: ""
+  })
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('http://localhost:8080/api/v1/user/register', user, {
+        headers: {
+          'Content-type': 'application/json'
+        },
+        withCrediantials: true
+      })
+      if (res.data.success) {
+        navigate("/login")
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      toast.error(error.response.data.message)
+      console.log(error)
+    }
+    setUser({
+      fullName: "",
+      username: "",
+      password: "",
+      confirmPassword: "",
+      gender: ""
+    })
+  }
+  const handleCheckBox = (gender) => {
+    setUser({ ...user, gender })
+  }
   return (
-    <div>SignUp</div>
+    <div className='min-w-96 mx-auto'>
+      <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100'>
+        <h1 className='text-3xl font-bold text-center'>Signup</h1>
+        <form onSubmit={onSubmitHandler} action="">
+          <div>
+            <label className="label p-2">
+              <span className='text-base label-text'>Full Name</span>
+            </label>
+            <input value={user.fullName} onChange={(e) => setUser({
+              ...user, fullName: e.target.value
+            })} className='w-full input input-bordered h-10' type="text" placeholder='' />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className='text-base label-text'>Username</span>
+            </label>
+            <input value={user.username} onChange={(e) => setUser({
+              ...user, username: e.target.value
+            })} className='w-full input input-bordered h-10' type="text" placeholder='' />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className='text-base label-text'>Password</span>
+            </label>
+            <input value={user.password} onChange={(e) => setUser({
+              ...user, password: e.target.value
+            })} className='w-full input input-bordered h-10' type="password" placeholder='' />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className='text-base label-text'>Confirm Password</span>
+            </label>
+            <input value={user.confirmPassword} onChange={(e) => setUser({
+              ...user, confirmPassword: e.target.value
+            })} className='w-full input input-bordered h-10' type="password" placeholder='' />
+          </div>
+          <div className='flex items-center my-4'>
+            <div className='flex items-center'>
+              <p>Male</p>
+              <input
+                type="checkbox"
+                checked={user.gender === "male"}
+                onChange={() => handleCheckBox("male")}
+                className="checkbox mx-2" />
+            </div>
+            <div className='flex items-center'>
+              <p>Female</p>
+              <input
+                type="checkbox"
+                checked={user.gender === "female"}
+                onChange={() => handleCheckBox("female")}
+                className="checkbox mx-2" />
+            </div>
+          </div>
+          <p className='text-center my-2'>Already have an account?  <Link className='text-center' to='/login'>Login</Link></p>
+          <div>
+            <button type='submit' className='btn btn-block btn-sm mt-2 border border-slate-700'>Signup</button>
+          </div>
+        </form>
+      </div>
+
+    </div>
   )
 }
 
